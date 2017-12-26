fa866cb8c4adac92cfa3cf7a837fa2f0a66145e8
Liuhaiyuan <lhy845876425@aliyun.com>
2017-12-26 17:02:55 +0800

    简单博客基本部署完成、准备进行用户管理使用

--- Python_work/django_work/django_work/blog_app/urls.py
+++ Python_work/django_work/django_work/blog_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.blog_app_title, name='blog_app_title')
+    url(r'^$', views.blog_app_title, name='blog_app_title'),
+    url(r'(?P<article_id>\d)/$', views.blog_app_article, name='blog_app_detail')
 ]
--- Python_work/django_work/django_work/blog_app/views.py
+++ Python_work/django_work/django_work/blog_app/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogArticles
 
 def blog_app_title(request):
     blogs = BlogArticles.objects.all()
     return render(request, "blog_app/titles.html", {"blogs":blogs})
+
+def blog_app_article(request, article_id):
+    # article = BlogArticles.objects.get(id=article_id)
+    article = get_object_or_404(BlogArticles, id=article_id)
+    pub = article.publish
+    return render(request, 'blog_app/content.html', {'article' : article, 'publish' : pub})
